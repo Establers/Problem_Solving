cc7d919328d2fda213140b0efc5af10efee2da58
Jaehwan Park <44702967+Establers@users.noreply.github.com>
2023-01-02 02:17:39 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 100 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -17,18 +17,18 @@ for idx_x in range(x-7) :
     for idx_y in range(y-7) :   # 8x8 접근 
         count = 0
         for i in range(8) : 
-            for j in range(8) :
-                if board[idx_x+i][idx_y+j] != pattern_WB[i][j] : 
-                    count += 1
+            for j in range(8) :  # 8x8 슬라이싱
+                if board[idx_x+i][idx_y+j] != pattern_WB[i][j] :  # 패턴WB와 비교 다르면 하나씩 증가
+                    count += 1  # 다른게 있을 때 마다 값을 누적해서 증가시킴 
         if (flag == 1) : 
-            ans = min(ans, count)
-        else : 
+            ans = min(ans, count)  # 8x8 비교가 끝나면 기존 값과 비교해서 작은 값을 넣기
+        else : # 처음에는 그냥 값넣기
             flag = 1
             ans = count
-        count = 0
+        count = 0  # WB와 비교해봤으니 BW와도 비교해보기..
         for i in range(8) : 
             for j in range(8) :
-                if board[idx_x+i][idx_y+j] != pattern_BW[i][j] : 
+                if board[idx_x+i][idx_y+j] != pattern_BW[i][j] :  # 패턴BW와 비교 다르면 하나씩 증
                     count += 1
         ans = min(ans, count)
 print(ans)
