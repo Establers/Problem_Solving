7df1272a372ab684f0b01f4931311dde0927d2b7
Jaehwan Park <44702967+Establers@users.noreply.github.com>
2023-01-02 02:59:52 +0900

    [Silver IV] Title: 수 찾기, Time: 160 ms, Memory: 49996 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -6,9 +6,12 @@ n = int(input())
 
 _list = list(map(int, input().split()))
 _set = set(_list)
-
+# set이 list보다 검색 시간이 빠르다 해쉬어쩌고 그래서 set 형식으로 했고
+# 또한 중복한 값을 제거해줘서 검색할 범위를 줄일 수 있음
 m = int(input())
 _mList = list(map(int, input().split()))
+# 순서가 고려되는 요소이기에 set은 하지 않았음
 
 for i in _mList : 
     print("1" if i in _set else "0")
+    # 들어 있는지 확인하기 위해 in을 사용하였음.
