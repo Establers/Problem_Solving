5e412a72552473402ff7df4f93bc7fecb90472f6
Jaehwan Park <44702967+Establers@users.noreply.github.com>
2023-01-26 22:08:49 +0900

    [Silver V] Title: 소수 찾기, Time: 36 ms, Memory: 32540 KB -BaekjoonHub

--- 백준/Silver/1978. 소수 찾기/소수 찾기.py
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.py
@@ -1,26 +1,26 @@
+import math
 import sys
-import math # sqrt
-N = int(sys.stdin.readline().rstrip()) # 받을 수의 개수
+input = sys.stdin.readline
 
-list_num = list(map(int, sys.stdin.readline().rstrip().split()))
+n = int(input())
+numList = list(map(int, input().split()))
 
-cnt = 0 # 소수 개수 ++
+d = [ True for i in range(1001)] # true 소수
 
-# 1은 소수가 아니니 list_num에서 제거
-if 1 in list_num :
-    list_num.remove(1)
+d[0:1] = False, False
 
-def issosu(number) :
-    # 2 부터 모든 수에 대해서 진행해도 되지만
-    # 소수 판별 방법이 그 수의 root 까지만 확인해도 됨
-    for i in range(2, int(math.sqrt(number)) + 1) :
-        if number % i == 0 :  # 어떤 수에 대해 나눠지면
-            return False  # 소수가 아니니 False 리턴
-    return True
+for i in range(2, int(math.sqrt(1000))+1) :
+    if d[i] == True :
+        for j in range(i+i, 1001, i) :
+            if d[j] == True :
+                d[j] = False
 
-for num in list_num : # 리스트에 포함된 num에 순서대로 접근
-    # 소수 확인
-    if (issosu(num) == True) : 
-        cnt += 1
+count = 0
+for i in numList :
+    if d[i] == True :
+        count += 1
 
-print(cnt)
+print(count)
+
+
+
