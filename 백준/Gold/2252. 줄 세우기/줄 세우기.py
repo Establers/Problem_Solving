7a2c6e23f8d4df1e75c877b20c0fdc02c20d9377
Jaehwan Park <44702967+Establers@users.noreply.github.com>
2023-01-28 00:57:16 +0900

    [Gold III] Title: 줄 세우기, Time: 200 ms, Memory: 38836 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -3,33 +3,29 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-
-graph = [[] for i in range(n + 1)]
-indegree = [0 for i in range(n + 1)]
+graph = [ [] for i in range(n+1) ]
+indegree = [0 for i in range(n+1)]
 result = []
 q = deque()
 
-for i in range(1, m + 1):
+for i in range(1, m+1) :
     a, b = map(int, input().split())
     graph[a].append(b)
     indegree[b] += 1
 
-
-def topology_sort():
-    for i in range(1, n + 1):
-        if (indegree[i] == 0):
+def topology_sort() :
+    for i in range(1, n+1) : # 진입 차수 0 인것 큐에 넣기
+        if (indegree[i] == 0) :
             q.append(i)
 
     while q:
-        stu = q.popleft()
-        result.append(stu)
+        now = q.popleft()
+        result.append(now)
 
-        for i in graph[stu]:
-            indegree[i] += -1
-            if (indegree[i] ==  0):
+        for i in graph[now] :
+            indegree[i] += -1 # 진입 차수 감소
+            if(indegree[i] == 0) : # 다시 0 이면 큐에 추가
                 q.append(i)
 
-
 topology_sort()
-
 print(*result, sep=' ')
