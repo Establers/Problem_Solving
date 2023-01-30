178e655582b431ffc3a751d9ac30dd2253102ba4
Jaehwan Park <44702967+Establers@users.noreply.github.com>
2023-01-30 20:40:13 +0900

    [Gold IV] Title: 부분합, Time: 136 ms, Memory: 41220 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -1,50 +1,36 @@
 import sys
 input = sys.stdin.readline
 
-N, S = map(int, input().split())
-
-list_ = list(map(int, input().split()))
-# 수가 들어있는 리스트
+n, s = map(int, input().split())
+nums = list(map(int, input().split()))
 
 pre_sum = [0]
-sum_value = 0
+temp = 0
+for i in nums :
+    temp += i
+    pre_sum.append(temp)
 
-# 연속된 수 이여서 구간 합.
-for i in list_ :
-    sum_value += i
-    pre_sum.append(sum_value)
+#print(pre_sum)
 
-# print(pre_sum)
-# 구간 합 완성
-# [0, 5, 6, 9, 14, 24, 31, 35, 44, 46, 54]
-
-# 구간 합을 뒤져가면서 S 넘고, 길이가 가장 짧은 것 찾기 min
-# 구간 합 탐색
 start = 0
 end = 0
-INF = 1e9
+INF = 1e9 + 100
 result = INF
+
 while(True) :
-    value = pre_sum[end] - pre_sum[start]
-    #print(value,start, end)
+    val = pre_sum[end] - pre_sum[start]
+    #print(val, result,start)
+    if (val >= s) : # 조건 만족
+        result = min(result, end-start)
+        if (start < n) :
+            start += 1 # 합을 더 작게
+    else :
+        if(end == n) : break
+        # s 보다 크지도 않고.. val을 더이상 크게할수도 읎음
+        if(end < n) :
+            end += 1 # 합을 더 크게(val을 더크게)
 
-    if (value >= S) :
-        #print(value, start, end)
-        result = min(result, end - start)
-        # print("결과1 : ", result)
-        start += 1
-        #print(start)
-    else : # 작다면
-        if (end == len(pre_sum) - 1):
-            break
-        if (end < len(pre_sum) - 1):
-            end += 1
-
-
-if result != INF :
-    print(result)
-else :
+if (result == INF) :
     print(0)
-
-#10 15
-#1 1 1 1 1 1 1 1 1 2
+else :
+    print(result)
