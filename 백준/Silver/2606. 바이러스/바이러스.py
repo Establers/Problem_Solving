b3d682a8ac942630e13bd3fae9a50830bd59cb7a
Jaehwan Park <44702967+Establers@users.noreply.github.com>
2023-02-08 23:38:30 +0900

    [Silver III] Title: 바이러스, Time: 64 ms, Memory: 34128 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,25 +1,35 @@
+# 2606
 import sys
 input = sys.stdin.readline
+from collections import deque
 
-n = int(input()) # 컴퓨터 수
-m = int(input()) # 직접 연결되어 있는 컴퓨터 쌍의 수 # 노드 수
-INF = int(1e9)
-graph = [ [INF] * (n+1) for i in range(n+1)]
+v = int(input())
+e = int(input())
+graph = [ [] for i in range(v+1)]
+visited = [ 0 for _ in range(v+1)]
 
-for _ in range(m) :
+for i in range(1, e+1) :
     a, b = map(int, input().split())
-    # a 노드 b 노드는 연결되어 있다.
-    graph[a][b] = 1
-    graph[b][a] = 1
+    graph[a].append(b)
+    graph[b].append(a)
+#print(graph)
+def bfs() :
+    q = deque()
+    q.append(1)
 
-for k in range(1, n+1) :
-    for a in range(1, n+1) :
-        for b in range(1, n+1) :
-            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+    while q :
+        now = q.popleft()
+        # print(now)
+        # visited[now] = 1
+        # if (graph[now]) :
+        #     q.append(graph[now].pop())
+        for i in graph[now] :
+            if not visited[i]:
+                q.append(i)
+                visited[i] = 1
 
-count = 0
-for i in graph[1] :
-    if i != INF :
-        count += 1
+bfs()
+#print(visited)
+print(visited.count(1) - 1)
 
-print(count-1)
+
