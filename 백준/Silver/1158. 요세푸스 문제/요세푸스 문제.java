ee867ba7d26ad9473fba5bf930599d6d4beffe2c
Jaehwan Park <44702967+Establers@users.noreply.github.com>
2023-02-13 15:51:12 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 272 ms, Memory: 14432 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,50 +1,59 @@
-
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
-import java.util.StringTokenizer;
 
 public class Main {
+	static int N,K;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer stk = new StringTokenizer(br.readLine());
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		int N = Integer.parseInt(stk.nextToken());
-		int K = Integer.parseInt(stk.nextToken());
-		int cnt = 0;
+		String[] str = br.readLine().split(" "); 
+		N=Integer.parseInt(str[0]);
+		K=Integer.parseInt(str[1]);
 		
-		LinkedList<Integer> linkedList = new LinkedList<Integer>();
+		List<Integer> list = new LinkedList<>();
 		
-		for(int i=1; i<=N; i++) {
-			linkedList.add(i);
+		for(int i=1;i<=N;i++) {
+			list.add(i);
 		}
 		
-		ListIterator<Integer> iterator = linkedList.listIterator(linkedList.size());
-
-		while(iterator.hasPrevious()) {
-			iterator.previous();
-		}
-		System.out.print("<");
-		while(!linkedList.isEmpty()) {
-			
-			for(int i=0; i<K; i++) {
-				if(iterator.hasNext()) {
-					iterator.next();
+		ListIterator<Integer> itr = list.listIterator();
+		
+		bw.write("<");
+		for(int i=0;list.size()>0 && i<N;i++) {
+			int k=-1;
+			for(int j=0;j<K;j++) {
+				
+				if(itr.hasNext()) {
+					k=itr.next().intValue();
 				} else {
-					for(int b=0; b<linkedList.size()-1; b++) {
-						iterator.previous();
-					}
+					itr = list.listIterator();
+					if(itr.hasNext())
+						k = itr.next().intValue();
+					else break;
 				}
+				
 			}
-			// System.out.println("제거할 값 : " + iterator.previous()); 
-			System.out.print(iterator.previous());
-			iterator.remove();
-			if(!linkedList.isEmpty()) System.out.print(", ");
+			itr.remove();
+			bw.write(String.valueOf(k));
+			if(list.size()==0) {
+				bw.write(">");
+				break;
+			}
+			bw.write(", ");
 		}
-		System.out.print(">");
+		bw.flush();
+		
+		
+		
+		
 		
 	}
+
 }
