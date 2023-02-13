b579453d01bab71eaecfa9f037461523b11cf39f
Jaehwan Park <44702967+Establers@users.noreply.github.com>
2023-02-13 15:23:08 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 584 ms, Memory: 20912 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,35 +1,50 @@
-// 요세푸스 문제
 
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stk = new StringTokenizer(br.readLine());
 		
-		int N = scanner.nextInt();
-		int M = scanner.nextInt();
+		int N = Integer.parseInt(stk.nextToken());
+		int K = Integer.parseInt(stk.nextToken());
 		int cnt = 0;
 		
-		Queue<Integer> queue = new LinkedList<Integer>();
+		LinkedList<Integer> linkedList = new LinkedList<Integer>();
 		
 		for(int i=1; i<=N; i++) {
-			queue.add(i);
-		}
-		System.out.print("<");
-		while(!queue.isEmpty()) {
-			for(int i=0; i<M-1; i++) {
-				if(!queue.isEmpty()) {
-					queue.add(queue.poll());
-				}
-			}
-			
-			System.out.print(queue.poll());
-			if(!queue.isEmpty()) System.out.print(", ");
+			linkedList.add(i);
 		}
 		
+		ListIterator<Integer> iterator = linkedList.listIterator(linkedList.size());
+
+		while(iterator.hasPrevious()) {
+			iterator.previous();
+		}
+		System.out.print("<");
+		while(!linkedList.isEmpty()) {
+			
+			for(int i=0; i<K; i++) {
+				if(iterator.hasNext()) {
+					iterator.next();
+				} else {
+					for(int b=0; b<linkedList.size()-1; b++) {
+						iterator.previous();
+					}
+				}
+			}
+			// System.out.println("제거할 값 : " + iterator.previous()); 
+			System.out.print(iterator.previous());
+			iterator.remove();
+			if(!linkedList.isEmpty()) System.out.print(", ");
+		}
 		System.out.print(">");
+		
 	}
 }
