2ed5ddac220f847ea6c63b3c9cb7e1849f568b9f
Jaehwan Park <44702967+Establers@users.noreply.github.com>
2023-02-14 17:37:22 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 228 ms, Memory: 117664 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,4 +1,7 @@
-# swea 4008 과 똑같음 
+import sys
+input = sys.stdin.readline
+
+# swea 4008 과 똑같음
 def dfs(math : int, cal, depth) :
     global n
     global result_max
@@ -10,28 +13,33 @@ def dfs(math : int, cal, depth) :
         result_min = min(result_min, cal)
         return
 
-    for i in range(4) :
-        if (math_list[i] > 0):
-            if (i == 0):
-                math_list[i] += -1
-                temp = cal + number_list[depth + 1]
-                dfs(i, temp, depth + 1)
-                math_list[i] += 1
-            elif (i == 1):
-                math_list[i] += -1
-                temp = cal - number_list[depth + 1]
-                dfs(i, temp, depth + 1)
-                math_list[i] += 1
-            elif (i == 2):
-                math_list[i] += -1
-                temp = cal * number_list[depth + 1]
-                dfs(i, temp, depth + 1)
-                math_list[i] += 1
-            elif (i == 3):
-                math_list[i] += -1
-                temp = (int)(cal / number_list[depth + 1])
-                dfs(i, temp, depth + 1)
-                math_list[i] += 1
+
+    if (math_list[0] > 0):
+        math_list[0] += -1
+        temp = cal + number_list[depth + 1]
+        dfs(0, temp, depth + 1)
+        math_list[0] += 1
+
+
+    if (math_list[1] > 0):
+        math_list[1] += -1
+        temp = cal - number_list[depth + 1]
+        dfs(1, temp, depth + 1)
+        math_list[1] += 1
+
+
+    if (math_list[2] > 0):
+        math_list[2] += -1
+        temp = cal * number_list[depth + 1]
+        dfs(2, temp, depth + 1)
+        math_list[2] += 1
+
+
+    if (math_list[3] > 0):
+        math_list[3] += -1
+        temp = (int)(cal / number_list[depth + 1])
+        dfs(3, temp, depth + 1)
+        math_list[3] += 1
 
 
 
@@ -41,10 +49,6 @@ math_list = list(map(int, input().split()))
 result_min = int(1e9)
 result_max = int(-1e9)
 
-for i in range(4):
-    dfs(i, number_list[0], 0)
+dfs(0, number_list[0], 0)
 
 print(result_max, result_min, sep='\n')
-
-
-
