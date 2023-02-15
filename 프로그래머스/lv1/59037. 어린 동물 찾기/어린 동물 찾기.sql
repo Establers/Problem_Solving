a120aa8cfeb562cc2a271fec05985e0fd7f2e528
Jaehwan Park <44702967+Establers@users.noreply.github.com>
2023-02-15 10:40:38 +0900

    [level 1] Title: 어린 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv1/59037. 어린 동물 찾기/어린 동물 찾기.sql
+++ 프로그래머스/lv1/59037. 어린 동물 찾기/어린 동물 찾기.sql
@@ -1,5 +1,6 @@
 -- 코드를 입력하세요
-SELECT ANIMAL_ID, NAME
-FROM animal_ins
-where intake_condition != 'Aged'
-order by animal_id
+SELECT animal_id, name
+from animal_ins
+where intake_condition not like 'Aged'
+order by 1
+;
