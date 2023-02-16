3dedcfb87cc3f421dcc29739a356e612644b567a
Jaehwan Park <44702967+Establers@users.noreply.github.com>
2023-02-16 10:33:03 +0900

    [Gold V] Title: 암호 만들기, Time: 132 ms, Memory: 115276 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -1,35 +1,19 @@
+import itertools
+
 moum = ["a", "e", "i", "o", "u"]
 
 L, C = map(int, input().split())
 str_list = list(input().split())
 str_list.sort()
 
-# print(str_list)
-
 # a..b...c..d..e 증가하는 순서로 넣어야 하고
-# 최소 한개의 모음이 포함되어야 한다
-temp = []
-visited = [ False for _ in range(C) ]
-result_list = [ 0 for _ in range(L) ]
+# 최소 한개의 모음이 포함되어야 한다 + 두개의 자음
 
-def backtracking(depth, flag, idx):
-    global result_list
-    if(depth == L):
-        count = 0
-        for i in result_list :
-            if (i in moum) :
-                count += 1
-        if(count >= 1 and L-count >= 2) :
-            print(*result_list, sep='')
-        return
+for perm_str in itertools.combinations(str_list, L) :
+    count = 0
+    for i in perm_str :
+        if i in moum :
+            count += 1
 
-    else :
-        for i in range(idx, C) :
-            if(flag & 1<<i) != 0 : continue
-            result_list[depth] = str_list[i]
-            backtracking(depth + 1, flag | (1<<i), i+1)
-
-backtracking(0,0,0)
-
-
-
+    if(count >= 1 and L-count >= 2) :
+        print(*list(perm_str), sep='')
