2f255e10edf626650df78b68264dac8bc2219a07
Jaehwan Park <44702967+Establers@users.noreply.github.com>
2023-02-17 11:37:17 +0900

    [Silver V] Title: 색종이, Time: 76 ms, Memory: 11688 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,40 +1,40 @@
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
+	static int N,result;
+	static boolean map[][];
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer stk = null;
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		N = Integer.parseInt(br.readLine());
+		map = new boolean[100][100];
+
 		
-		int n = Integer.parseInt(br.readLine());
-		int a,b = 0;
-		int count = 0;
-		
-		boolean[][] board = new boolean[100][100];
-		
-		for(int i=0; i<n; i++) {
-			stk = new StringTokenizer(br.readLine());
-			a = Integer.parseInt(stk.nextToken());
-			b = Integer.parseInt(stk.nextToken());
-			
-			for(int x=a-1; x<a+9; x++) {
-				for(int y=b-1; y<b+9; y++) {
-					if (x >= 100 || y >= 100) continue;
-					board[x][y] = true; 
+		StringTokenizer st;
+		int x, y;
+		for(int i=0;i<N;i++) {
+			st = new StringTokenizer(br.readLine());
+			x=Integer.parseInt(st.nextToken());
+			y=Integer.parseInt(st.nextToken());
+			for(int tmpy=y;tmpy<y+10;tmpy++) {
+				for(int tmpx=x;tmpx<x+10;tmpx++) {
+					map[tmpy][tmpx]=true;
 				}
 			}
-		
 		}
-		
-		for(int i=0; i<100; i++) {
-			for(int j=0; j<100; j++) {
-				if(board[i][j] == true) count++;
+		result=0;
+		for(int ty=0;ty<100;ty++) {
+			for(int tx=0;tx<100;tx++) {
+				if(map[ty][tx]) result++;
 			}
 		}
+		System.out.println(result);
 		
-		System.out.println(count);
-	}
+	}	
 }
