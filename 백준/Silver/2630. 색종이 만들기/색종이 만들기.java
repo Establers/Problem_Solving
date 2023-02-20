4c66fb56ded33c72b4ad31f6819d472eebc98d52
Jaehwan Park <44702967+Establers@users.noreply.github.com>
2023-02-20 16:10:39 +0900

    [Silver II] Title: 색종이 만들기, Time: 112 ms, Memory: 13208 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -1,72 +1,121 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int[][] arr;
-	
-	static int w_cnt = 0; // 0
-	static int b_cnt = 0; // 1
-	
-	public static void findPaper(int n, int x, int y) {
-		char value = checkAll(n,x,y);
-		if(value != 'n') {
-			if(value == 'b') {
-				b_cnt += 1;
-				return;
-			} else 
-			{
-				w_cnt += 1;
-				return;
+	static int N, numWhite, numBlue;
+	static final boolean BLUE = true, WHITE = false;
+	static boolean[][] Map;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		N = Integer.parseInt(br.readLine());
+		Map = new boolean[N][N];
+		for(int i=0;i<N;i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0;j<N;j++) {
+				if(st.nextToken().equals("1")) {
+					Map[i][j]=BLUE;
+				} //else Map[i][j]=WHITE;
 			}
 		}
-		
-		// check 완료
-		int nextNum = n/2;
-		findPaper(nextNum,	x,			y);
-		findPaper(nextNum,	x+nextNum,	y);
-		findPaper(nextNum,	x,			y+nextNum);
-		findPaper(nextNum,	x+nextNum,	y+nextNum);
-	}
-	
-	
-	/*
-	 * 모두가 1인지 0인지 return 
-	 * */
-	public static char checkAll(int n, int x, int y) {
-		int temp = arr[x][y]; // 좌상단 값 (0 또는 1)
-		
-		for(int i=x; i<x+n; i++) {
-			for(int j=y; j<y+n; j++) {
-				if(temp != arr[i][j]) {
-					return 'n'; // 다른게 있다
+		boolean isDividable=false; 
+		boolean previous = Map[0][0];
+		for(int i=0;i<N && !isDividable;i++) {
+			for(int j=0;j<N;j++) {
+				if(isDividable = (previous != Map[i][j])) {
+					break;
 				}
 			}
 		}
-
-		// 모두가 같다.
-		if(temp == 1) return 'b';
-		else return 'w'; 
-	}
-	
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		arr = new int[n][n];
-		StringTokenizer stk = null;
-		
-		for(int i=0; i<n; i++) {
-			stk = new StringTokenizer(br.readLine()," ");
-			for(int j=0; j<n; j++) {
-				arr[i][j] = Integer.parseInt(stk.nextToken());
-			}
+		if(isDividable)
+			checkColor(N,0,0);
+		else {
+			if(previous) numBlue++;
+			else numWhite++;
 		}
 		
-		findPaper(n, 0, 0);
-		
-		System.out.println(w_cnt);
-		System.out.println(b_cnt);
+		System.out.println(numWhite);
+		System.out.println(numBlue);
 	}
+	
+	private static void checkColor(int n,int startX, int startY) {
+		n=n/2;
+		boolean isDividable, previous;
+		//(isDividable == true) = 나눌 수 있으면 = 사각형 내부 요소들이 다르면 > 비교 중지하고 나누기 시작
+		//(isDividable == false) = 나눌 수 없으면 = 사각형 내부 요소들이 다 같다면 > previous에 해당하는 색 ++, 분할 X
+		
+		//첫번째 사각형
+		isDividable=false; 
+		previous = Map[startY][startX];
+		for(int i=startY;i-startY<n && !isDividable;i++) {
+			for(int j=startX;j-startX<n;j++) {
+				if(isDividable = (previous != Map[i][j])) {
+					break;
+				}
+			}
+		}
+		if(isDividable) {
+			checkColor(n,startX,startY);
+		} else {
+			if(previous) numBlue++;
+			else numWhite++;
+		}		
+		
+		//두번째 사각형
+		isDividable=false;
+		previous = Map[startY+n][startX];
+		for(int i=startY+n;i-startY-n<n && !isDividable;i++) {
+			for(int j=startX;j-startX<n;j++) {
+				if(isDividable = (previous != Map[i][j])) {
+					break;
+				}
+			}
+		}
+		if(isDividable) {
+			checkColor(n,startX,startY+n);
+		} else {
+			if(previous) numBlue++;
+			else numWhite++;
+		}		
+		
+		
+		//세번째 사각형
+		isDividable=false;
+		previous = Map[startY][startX+n];
+		for(int i=startY;i-startY<n && !isDividable;i++) {
+			for(int j=startX+n;j-startX-n<n;j++) {
+				if(isDividable = (previous != Map[i][j])) {
+					break;
+				}
+			}
+		}
+		if(isDividable) {
+			checkColor(n,startX+n,startY);
+		} else {
+			if(previous) numBlue++;
+			else numWhite++;
+		}		
+
+		
+		//네번째 사각형
+		isDividable=false;
+		previous = Map[startY+n][startX+n];
+		for(int i=startY+n;i-startY-n<n && !isDividable;i++) {
+			for(int j=startX+n;j-startX-n<n;j++) {
+				if(isDividable = (previous != Map[i][j])) {
+					break;
+				}
+			}
+		}
+		if(isDividable) {
+			checkColor(n,startX+n,startY+n);
+		} else {
+			if(previous) numBlue++;
+			else numWhite++;
+		}		
+		
+	}
+	
 }
