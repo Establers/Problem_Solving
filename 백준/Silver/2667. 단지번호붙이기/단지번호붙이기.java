5b23f63fb851a51b9d55319b151e1cb087cc236c
Jaehwan Park <44702967+Establers@users.noreply.github.com>
2023-02-22 10:50:10 +0900

    [Silver I] Title: 단지번호붙이기, Time: 240 ms, Memory: 18424 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,53 +1,51 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class Main {
 	static int N, cnt;
-	static char[][] map;
-	static boolean[][] calced;
+	static boolean[][] map;
 	static List<Integer> list ;
-	static List<int[]> houses ;
+	static Queue<int[]> houses ;
 	static int[] dirX = {0,0,-1,1};
 	static int[] dirY = {-1,1,0,0};
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		N = Integer.parseInt(br.readLine());
-		map = new char[N][N];
-		calced = new boolean[N][N];
+		map = new boolean[N][N];
 		list = new ArrayList<>();
 		houses = new LinkedList<>();
 		
+		char[] arr;
 		for(int i=0;i<N;i++) {
-			map[i] = br.readLine().toCharArray();
+			arr = br.readLine().toCharArray();
+			for(int j=0;j<N;j++) {
+				map[i][j] = arr[j]=='1'?true:false;
+			}
 		}
 		
 		int x,y,nx,ny;
 		for(int i=0;i<N;i++) {
 			for(int j=0;j<N;j++) {
 				cnt=0;
-				if(map[i][j]=='1') {
-					map[i][j]='0';
+				if(map[i][j]) {
+					map[i][j]=false;
 					cnt++;
 					houses.add(new int[] {j,i});
 					while(!houses.isEmpty()) {
-						x = houses.get(0)[0];
-						y = houses.get(0)[1];
-						houses.remove(0);
+						x = houses.peek()[0];
+						y = houses.poll()[1];
 						for(int k=0;k<4;k++) {
 							nx = x+dirX[k];
 							ny = y+dirY[k];
 							if(nx==N || nx<0 ||ny==N || ny<0) continue;
-							if(map[ny][nx]=='1') {
-								map[ny][nx]='0';
+							if(map[ny][nx]) {
+								map[ny][nx]=false;
 								cnt++;
 								houses.add(new int[] {nx,ny});
 							}
