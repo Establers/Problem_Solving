e913dfcdfc510a8489bc0e773481c2b517236a5b
Jaehwan Park <44702967+Establers@users.noreply.github.com>
2023-02-22 10:43:52 +0900

    [Silver I] Title: 단지번호붙이기, Time: 240 ms, Memory: 18428 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,68 +1,69 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
-	
-	static int n;
-	static int[] dx = {1, -1, 0, 0};
-	static int[] dy = {0, 0, 1, -1};
-	static int[][] board;
-	static Queue<int[]> q = new ArrayDeque<>();
-	static ArrayList<Integer> answers = new ArrayList<>();
-	static int danzi = 0;
-	static int counts = 0;
-	
-	static boolean[][] visited;
-	
-	public static void dfs(int x, int y, int count) {
-		
-		for(int i=0; i<4; i++) {
-			int nx = x + dx[i];
-			int ny = y + dy[i];
-			
-			if(0 <= nx && nx < n && 0 <= ny && ny < n) {
-				// 경계안
-				if(!visited[nx][ny] && board[nx][ny] != 0) {
-					counts++;
-					visited[nx][ny] = true;
-					dfs(nx, ny, count+1);
-				} 
-			} 
-		}
-	}
-	
+	static int N, cnt;
+	static char[][] map;
+	static boolean[][] calced;
+	static List<Integer> list ;
+	static List<int[]> houses ;
+	static int[] dirX = {0,0,-1,1};
+	static int[] dirY = {-1,1,0,0};
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		N = Integer.parseInt(br.readLine());
+		map = new char[N][N];
+		calced = new boolean[N][N];
+		list = new ArrayList<>();
+		houses = new LinkedList<>();
 		
-		n = Integer.parseInt(br.readLine());
-		board = new int[n][n];
-		visited = new boolean[n][n];
-		
-		for(int i=0; i<n; i++) {
-			String temp = br.readLine();
-			for(int j=0; j<n; j++) {
-				board[i][j] = temp.charAt(j) - '0';
-			}
+		for(int i=0;i<N;i++) {
+			map[i] = br.readLine().toCharArray();
 		}
 		
-		for(int i=0; i<n; i++) {
-			for(int j=0; j<n; j++) {
-				if(!visited[i][j] && board[i][j] != 0) {
-					counts = 1;
-					visited[i][j] = true; 
-					dfs(i, j, 0);
-					answers.add(counts);
+		int x,y,nx,ny;
+		for(int i=0;i<N;i++) {
+			for(int j=0;j<N;j++) {
+				cnt=0;
+				if(map[i][j]=='1') {
+					map[i][j]='0';
+					cnt++;
+					houses.add(new int[] {j,i});
+					while(!houses.isEmpty()) {
+						x = houses.get(0)[0];
+						y = houses.get(0)[1];
+						houses.remove(0);
+						for(int k=0;k<4;k++) {
+							nx = x+dirX[k];
+							ny = y+dirY[k];
+							if(nx==N || nx<0 ||ny==N || ny<0) continue;
+							if(map[ny][nx]=='1') {
+								map[ny][nx]='0';
+								cnt++;
+								houses.add(new int[] {nx,ny});
+							}
+						}
+					}
+					list.add(cnt);
 				}
 			}
 		}
+		list.sort((o1,o2)->o1-o2);
 		
-		
-		Collections.sort(answers);
-		System.out.println(answers.size());
-		for(int h : answers) {
-			System.out.println(h);
+		System.out.println(list.size());
+		for(int a : list) {
+			System.out.println(a);
 		}
-
+		
+		
 	}
 }
