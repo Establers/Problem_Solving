b88a497c8548d95f7c17757a3af47595a2270ffb
Jaehwan Park <44702967+Establers@users.noreply.github.com>
2023-02-23 16:22:33 +0900

    [Gold IV] Title: 알파벳, Time: 856 ms, Memory: 15408 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,81 +1,55 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
-class Point{
-	public int x;
-	public int y;
-	public String nli;
-	public Point(int x, int y,  String nli) {
-		this.x = x;
-		this.y = y;
-		this.nli = nli;
-	}
-}
-
 public class Main {
-	static int R;
-	static int C;
+	static int R, C;
 	static int[][] board;
-	static boolean[] visited;
-	static int[] dx = {-1,1,0,0};
-	static int[] dy = {0,0,-1,1};
-	static int count = 1;
+	static boolean[] visited; 
+	static int answer;
 	
+	static int[] dx = {1, -1, 0, 0};
+	static int[] dy = {0, 0, 1, -1};
 	
-	public static void dfs(int a, int b, int depth) {
-		count = Math.max(count, depth);
-		
+	public static void dfs(int x, int y, int length) {
+		answer = Math.max(answer, length);
+		visited[board[x][y]] = true; 
 		for(int i=0; i<4; i++) {
-			int nx = a+dx[i];
-			int ny = b+dy[i];
-				if(!inTheBoundary(nx, ny)) {
-					continue;
-				} else {
-					if(!visited[(board[nx][ny])]) {
-						visited[(board[nx][ny])] = true;
-						dfs(nx, ny, depth+1);
-						visited[(board[nx][ny])] = false;
-					}
-					
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			
+			if(0<= nx && nx < R && 0<= ny && ny < C) {
+				if(!visited[board[nx][ny]]) {
+					visited[board[nx][ny]]=true;
+					dfs(nx, ny, length+1);
+					visited[board[nx][ny]]=false;
 				}
+			}
+			
 		}
 	}
-
-
-	public static boolean inTheBoundary(int nx, int ny) {
-		if(nx < 0 || nx >= R || ny < 0 || ny >= C) {
-			return false;
-		}
-		return true;
-	}
-
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		R = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
+		StringTokenizer stk = new StringTokenizer(br.readLine());
+		
+		R = Integer.parseInt(stk.nextToken());
+		C = Integer.parseInt(stk.nextToken());
+		
 		board = new int[R][C];
-		visited = new boolean[26];
-		//System.out.println(Integer.parseInt('Z'));
+		visited = new boolean[27]; // A~Z
 		
 		for(int i=0; i<R; i++) {
 			String temp = br.readLine();
 			for(int j=0; j<C; j++) {
-				board[i][j] = temp.charAt(j) - 'A'; // A : 0 .. 
+				board[i][j] = temp.charAt(j) - 'A'; 
 			}
 		}
-		visited[(board[0][0])] = true;
-		dfs(0,0,1);
-
+		// Input end
 		
-		System.out.println(count);
+		dfs(0, 0, 1);
+		System.out.println(answer);
+	
 	}
 }
-
