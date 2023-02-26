b2b51c0490fca94a1f6cbf3e6f488637474d3273
Jaehwan Park <44702967+Establers@users.noreply.github.com>
2023-02-26 11:36:14 +0900

    [Silver V] Title: 막대기, Time: 76 ms, Memory: 11460 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -0,0 +1,24 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		int count = 0;
+		for(int i=0; i<=6; i++) {
+			if((n & (1<<i)) > 0) {
+				count += 1;
+			}
+		}
+		System.out.println(count);
+		
+		
+		
+	}
+}
