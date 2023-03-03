89333d00bf20a369bfad0c0fedb2549a5bd12116
Jaehwan Park <44702967+Establers@users.noreply.github.com>
2023-03-04 00:04:47 +0900

    [Gold V] Title: 토마토, Time: 424 ms, Memory: 166948 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,62 +1,51 @@
 import sys
 input = sys.stdin.readline
 from collections import deque
+m, n = map(int, input().split())
 
-m, n = map(int ,input().split())
-graph = []
+board = []
 
-for i in range(n) :
-    list_ = list(map(int, input().split()))
-    graph.append(list_)
-    #graph[0~3][0~5] # 예제1
-    #graph[n][m]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+for _ in range(n) :
+    board.append(list(map(int, input().split())))
+
+# 짱멋진 BFS로 10분컷 해보기~ 23:55 시작
+
+visited = [ [ False for _ in range(m)] for _ in range(n)]
+
 q = deque()
 
-dx = [1,-1, 0, 0]
-dy = [0, 0, 1,-1]
+for i in range(n) :
+    for j in range(m) :
+        if board[i][j] == 1:
+            q.append( (i, j) )
 
-def bfs() :
-    # 1. 1 찾아서 큐에 넣기
-    for i in range(m) :
-        for j in range(n) :
-            if graph[j][i] == 1 :
-                q.append( (j,i) )
+while q :
+    x, y = q.popleft()
 
-    # 1-2. 큐가 빌때까지 위 과정 진행
-    while q :
-        #print(q)
-        y, x = q.popleft()
+    for i in range(4) :
+        nx = x + dx[i]
+        ny = y + dy[i]
 
-        #print(y,x)
-        count = 0
-        for i in range(4) : # dx dy
-            nx = x + dx[i] # m
-            ny = y + dy[i] # n
-            if (0 <= nx < m and 0 <= ny < n) == False :
-                continue # 맵 밖.
+        if 0 > nx or 0 > ny or nx >= n or ny >= m : continue
+        if board[nx][ny] == -1 : continue
+        if board[nx][ny] == 0 :
+            board[nx][ny] = board[x][y] + 1
+            q.append( (nx , ny))
 
-            # 2. 1 상하좌우 0-> 1로 만들고 다시 큐에 넣기 (범위 안넘치게)
-            else :
-                count += 1
-                if graph[ny][nx] == 0:
-                    graph[ny][nx] = graph[y][x] + 1
-                    q.append((ny, nx))
-
-                # 3. 단 -1 일 경우, 변경X
-                elif graph[ny][nx] == -1:
-                    continue
-
-# 4. 큐가 비었고, 만약 맵에 0이 있다면, -1
+# print(*board, sep='\n')
 def solution() :
-    count_day = 0
-    bfs()
-    for i in graph :
-        for j in i :
-            if j == 0:
+    answer = 0
+
+    for i in range(n) :
+        for j in range(m) :
+            answer = max(answer, board[i][j])
+            if board[i][j] == 0 :
                 print(-1)
                 return
 
-        count_day = max(count_day, max(i))
-    print(count_day - 1)
+    print(answer - 1)
 
 solution()
