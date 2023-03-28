a407213a8802324f40574138e8972a3ac1da3acd
Jaehwan Park <44702967+Establers@users.noreply.github.com>
2023-03-29 02:12:16 +0900

    [Gold V] Title: 도서관, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/1461. 도서관/도서관.py
+++ 백준/Gold/1461. 도서관/도서관.py
@@ -5,25 +5,24 @@ n,m = map(int, input().split())
 
 a = list(map(int, input().split()))
 
-upper_case = []
-lower_case = []
+u_c = []
+l_c = []
 
 for i in range(n) :
     if a[i] > 0 :
-        upper_case.append(a[i])
+        u_c.append(a[i])
     else  :
-        lower_case.append(-a[i])
+        l_c.append(-a[i])
 
 umx, lmx = 0, 0
-if upper_case != [] :
-    upper_case.sort()
-    umx = upper_case[-1]
-if lower_case != [] :
-    lower_case.sort()
-    lmx = lower_case[-1]
+if u_c != [] :
+    u_c.sort()
+    umx = u_c[-1]
+if l_c != [] :
+    l_c.sort()
+    lmx = l_c[-1]
 
 dt_max = max(umx,lmx)
-# to be substract
 
 # 오른쪽 끝에서 부터 선택 m개
 result = 0
@@ -32,27 +31,6 @@ def select2(list) :
     for i in range(len(list)-1, -1, -m) :
         result += list[i] * 2
 
-# select(upper_case)
-# select(lower_case)
-
-select2(upper_case)
-select2(lower_case)
-result -= dt_max
-print(result)
-
-"""
-def select(list) :
-    global result
-    cnt = 0
-    temp = []
-
-    for i in range(len(list)-1, -1, -1) :
-        cnt += 1
-        temp.append(list[i])
-        if cnt >= m :
-            result += max(temp) * 2
-            temp = []; cnt = 0;
-
-    if temp != [] :
-        result += max(temp) * 2
-"""
+select2(u_c)
+select2(l_c)
+print(result - dt_max)
