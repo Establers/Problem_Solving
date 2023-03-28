1156c49273c8955ceef54765cd43e7a96bfbf6fd
Jaehwan Park <44702967+Establers@users.noreply.github.com>
2023-03-29 01:59:52 +0900

    [Gold V] Title: 도서관, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Gold/1461. 도서관/도서관.py
@@ -0,0 +1,48 @@
+import sys
+input = sys.stdin.readline
+
+n,m = map(int, input().split())
+
+a = list(map(int, input().split()))
+
+upper_case = []
+lower_case = []
+
+for i in range(n) :
+    if a[i] > 0 :
+        upper_case.append(a[i])
+    else  :
+        lower_case.append(-a[i])
+
+umx, lmx = 0, 0
+if upper_case != [] :
+    upper_case.sort()
+    umx = upper_case[-1]
+if lower_case != [] :
+    lower_case.sort()
+    lmx = lower_case[-1]
+
+dt_max = max(umx,lmx)
+# to be substract
+
+# 오른쪽 끝에서 부터 선택 m개
+result = 0
+def select(list) :
+    global result
+    cnt = 0
+    temp = []
+
+    for i in range(len(list)-1, -1, -1) :
+        cnt += 1
+        temp.append(list[i])
+        if cnt >= m :
+            result += max(temp) * 2
+            temp = []; cnt = 0;
+
+    if temp != [] :
+        result += max(temp) * 2
+
+select(upper_case)
+select(lower_case)
+result -= dt_max
+print(result)
