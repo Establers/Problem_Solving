ccc00e50ff8a3a15b5df3dd9d45830d4b8606f49
Jaehwan Park <44702967+Establers@users.noreply.github.com>
2023-03-29 02:10:22 +0900

    [Gold V] Title: 도서관, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/1461. 도서관/도서관.py
+++ 백준/Gold/1461. 도서관/도서관.py
@@ -27,6 +27,20 @@ dt_max = max(umx,lmx)
 
 # 오른쪽 끝에서 부터 선택 m개
 result = 0
+def select2(list) :
+    global result
+    for i in range(len(list)-1, -1, -m) :
+        result += list[i] * 2
+
+# select(upper_case)
+# select(lower_case)
+
+select2(upper_case)
+select2(lower_case)
+result -= dt_max
+print(result)
+
+"""
 def select(list) :
     global result
     cnt = 0
@@ -41,8 +55,4 @@ def select(list) :
 
     if temp != [] :
         result += max(temp) * 2
-
-select(upper_case)
-select(lower_case)
-result -= dt_max
-print(result)
+"""
