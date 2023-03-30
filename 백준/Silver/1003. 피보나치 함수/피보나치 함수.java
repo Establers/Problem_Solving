aede8a0480608e1a8548388c37f78b51f9622f17
Jaehwan Park <44702967+Establers@users.noreply.github.com>
2023-03-30 10:18:17 +0900

    [Silver III] Title: 피보나치 함수, Time: 216 ms, Memory: 17704 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -1,39 +1,37 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-
+import java.util.Scanner;
+ 
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+	static Integer[][] dp = new Integer[41][2];
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
 		
-		int t = Integer.parseInt(br.readLine());
+		dp[0][0] = 1;	// N=0 일 때의 0 호출 횟수
+		dp[0][1] = 0;	// N=0 일 때의 1 호출 횟수
+		dp[1][0] = 0;	// N=1 일 때의 0 호출 횟수
+		dp[1][1] = 1;	// N=1 일 때의 1 호출 횟수
 		
-		int[] zero = new int[41];
-		int[] one = new int[41];
-		
-		zero[0] = 1;
-		zero[1] = 0;
-		
-		one[0] = 0;
-		one[1] = 1;
-		
-		for (int i=0; i<t; i++) {
-			int n = Integer.parseInt(br.readLine());
-			
-			if(n < 2) {
-				if (n == 0) {
-					System.out.printf("%d %d\n",zero[0], one[0]);
-				} else if(n == 1) {
-					System.out.printf("%d %d\n",zero[1], one[1]);
-				}
-			} else {
-				for (int j=2; j<=n; j++) {
-					zero[j] = zero[j-1] + zero[j-2];
-					one[j] = one[j-1] + one[j-2];
-				}
-				System.out.printf("%d %d\n", zero[n], one[n]);
-			}
+		int T = in.nextInt();
+        
+		while(T-- > 0){
+			int N = in.nextInt();
+			fibonacci(N);
+			System.out.println(dp[N][0] + " " + dp[N][1]);
 		}
+		
 	}
+	
+	static Integer[] fibonacci(int N) {
+		// N에 대한 0, 1의 호출 횟수가 없을 떄(탐색하지 않은 값일 때)
+		if(dp[N][0] == null || dp[N][1] == null) {
+			// 각 N에 대한 0 호출 횟수와 1 호출 횟수를 재귀호출한다.
+			dp[N][0] = fibonacci(N - 1)[0] + fibonacci(N - 2)[0];
+			dp[N][1] = fibonacci(N - 1)[1] + fibonacci(N - 2)[1];
+		}
+		// N에 대한 0과 1, 즉 [N][0]과 [N][1] 을 담고있는 [N]을 반환한다.
+		return dp[N];
+ 
+	}
+ 
 }
