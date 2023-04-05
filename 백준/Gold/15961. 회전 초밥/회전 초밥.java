34eb083eacdd8513efa5177930b68ab5dd1dfb7d
Jaehwan Park <44702967+Establers@users.noreply.github.com>
2023-04-05 15:28:19 +0900

    [Gold IV] Title: 회전 초밥, Time: 560 ms, Memory: 170800 KB -BaekjoonHub

---
+++ 백준/Gold/15961. 회전 초밥/회전 초밥.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N, D, K, C, max;
+	static int[] belt, visited;
+
+	public static void main(String[] args) throws IOException {
+		init();
+		System.out.println(findAnswer());
+	}
+
+	private static void init() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		D = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+
+		belt = new int[N];
+		visited = new int[D + 1];
+
+		for (int i = 0; i < N; i++) {
+			belt[i] = Integer.parseInt(br.readLine());
+		}
+	}
+
+	private static int findAnswer() {
+		int count = 0;
+		int max = 0;
+
+		for (int i = 0; i < K; i++) {
+			if (visited[belt[i]] == 0) {
+				count++;
+			}
+			visited[belt[i]]++;
+		}
+
+		max = count;
+
+		for (int i = 1; i < N; i++) {
+			if (max <= count) {
+				max = count;
+				if(visited[C] == 0) {
+					max++;
+				}
+			}
+			visited[belt[i - 1]]--;
+			if(visited[belt[i - 1]] == 0) {
+				count--;
+			}
+			
+			if(visited[belt[(i + K -1) % N]] == 0) {
+				count++;
+			}
+			visited[belt[(i + K -1) % N]]++;
+		}
+		return max;
+	}
+}
